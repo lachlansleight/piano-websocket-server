76750fb281e2a337ecbca32574eaeae0325ca580
LachlanSleight <lachlansleight@gmail.com>
2025-05-10 13:19:50 +0100

    feat: Format + add default midi device to .env

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import http = require("http");
-import express = require("express");
+import * as http from "http";
+import * as express from "express";
 import { Server } from "socket.io";
 import MidiListener from "./lib/MidiListener";
+import * as dotenv from "dotenv";
 
-require("dotenv").config();
+dotenv.config();
 
 console.log("Creating express app");
 const app = express();
@@ -11,45 +12,45 @@ console.log("Creating webserver");
 const server = http.createServer(app);
 
 app.get("/", (req, res) => {
-    res.json({foo: "bar"});
+  res.json({ foo: "bar" });
 });
 
 console.log("Creating websocket server");
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 const devices = MidiListener.getDevices();
 console.log(devices);
 const midiListener = new MidiListener(
-    devices.inputs[1],
-    message => {
-        io.emit("noteOn", message);
-        console.log("noteOn", message);
-    },
-    message => {
-        io.emit("noteOff", message);
-        console.log("noteOff", message);
-    },
-    message => {
-        io.emit("cc", message);
-        console.log("cc", message);
-    },
+  process.env.DEVICENAME || devices.inputs[0],
+  (message) => {
+    io.emit("noteOn", message);
+    console.log("noteOn", message);
+  },
+  (message) => {
+    io.emit("noteOff", message);
+    console.log("noteOff", message);
+  },
+  (message) => {
+    io.emit("cc", message);
+    console.log("cc", message);
+  }
 );
 
-io.on("connection", socket => {
-    console.log("Client connected");
-    socket.emit("greeting", "Hello Friend");
+io.on("connection", (socket) => {
+  console.log("Client connected");
+  socket.emit("greeting", "Hello Friend");
 
-    io.on("disconnect", () => {
-        console.log("Client disconnected");
-    });
+  io.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
 });
 
 const PORT = process.env.PORT || 3003;
 server.listen(PORT, () => {
-    console.log("Server listening on port " + PORT);
+  console.log("Server listening on port " + PORT);
 });
